1fcad6d79c412d6a9d707d0d278713c93da94f4d
bobicez <bobicemj@gmail.com>
2025-07-11 19:22:53 +0200

    -created initial upload_page

---
+++ src/app/upload_page/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/header/Header";
+import Head from "next/head";
+
+export default function UploadPage() {
+    return (
+    <html>
+        <main>
+            <Header/>
+            <h1>Upload Post</h1>
+            <h2>Title</h2>
+            <p>
+                <img src="" alt="Upload image" />
+                Upload image (optional)</p>
+        </main>
+    </html>
+    )
+}
