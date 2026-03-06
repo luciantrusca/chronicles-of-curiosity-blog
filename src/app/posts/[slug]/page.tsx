006277a4b181f723edf6e1661590283010748ce5
bobicez <bobicemj@gmail.com>
2026-03-06 21:12:56 +0100

    feat: enhance PostPage to fetch and display individual posts with markdown support

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,19 @@
-import { fetchPost } from "@/app/lib/api";
+import { fetchPost, fetchPosts } from "@/app/lib/api";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+export async function generateStaticParams() {
+    const posts = await fetchPosts();
+    return posts.map(post => ({ slug: post.slug }));
+}
 
 export default async function PostPage({ params }: { params: { slug: string } }) {
     const post = await fetchPost(params.slug);
-    // const post = posts.find((p) => p.slug === params.slug);
-    return ( 
-    <div>
-        <h1>Post: {post.title}</h1>
-        <p>{post.content}</p>
-    </div>
-    
+
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown>
+        </div>
     );
 }
