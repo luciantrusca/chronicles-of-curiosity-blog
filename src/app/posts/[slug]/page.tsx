5e54f6e3fef8caec42eb8446e15f42a85698e890
bobicez <bobicemj@gmail.com>
2026-03-06 19:46:21 +0100

    feat: implement PostPage component to fetch and display individual posts

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { fetchPost, fetchPosts } from "@/app/lib/api";
+
+export default async function PostPage({ params }: { params: { slug: string } }) {
+    const post = await fetchPost(params.slug);
+    // const post = posts.find((p) => p.slug === params.slug);
+    return ( 
+    <div>
+        <h1>Post: {post.title}</h1>
+        <p>{post.content}</p>
+    </div>
+    
+    );
+}
