cc9ce192eb6292b80a65e75d4c8b336a53ac5d69
bobicez <bobicemj@gmail.com>
2026-03-06 21:54:37 +0100

    feat: wrap post titles in Link component for navigation

--- src/components/main_section/LatestPost.tsx
+++ src/components/main_section/LatestPost.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/app/lib/api";
+import Link from "next/link";
 
 export default function LatestPost({post, className}: {post: Post; className?: string[]}) {
     const preview = post.content.slice(0, 100) + "...";
@@ -6,7 +7,9 @@ export default function LatestPost({post, className}: {post: Post; className?: s
     return (
         <section className={`bg-[#F7E6B8] border-2 border-[#6B7133] p-4 ${className?.join(" ")}`}>
             <h1 className="text-lg font-bold">Latest Post</h1>
-            <h1>{post.title}</h1>
+            <Link href={`/posts/${post.slug}`}>
+                <h1>{post.title}</h1>
+            </Link>
             <div style={{ display: "flex", gap: "1rem" }}>
             {post.coverUrl && (<img className="w-45 p-4" src={post.coverUrl} alt={post.title} />)}
                 <article>{preview}</article>
--- src/components/main_section/PostCard.tsx
+++ src/components/main_section/PostCard.tsx
@@ -1,11 +1,14 @@
 import { Post } from "@/app/lib/api";
+import Link from "next/link";
 
 export default function PostCard({post}: {post: Post}) {
 
     return (
         <section className="flex flex-col max-w-50 w-35 items-center border-2 p-3 rounded-lg border-[#5B614E] bg-[#EFD9A1]">
             {post.coverUrl && <img className="w-15 m-2" src={post.coverUrl}/>}
-            <h2 className="font-bold">{post.title}</h2>
+            <Link href={`/posts/${post.slug}`}>
+                <h2 className="font-bold">{post.title}</h2>
+            </Link>
             <p>{new Date(post.updatedAt).toLocaleDateString()}</p>
         </section>
     )
